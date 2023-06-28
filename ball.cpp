2f0b54d86fa7c2b4c595002d6bb6fd1372783580
Aiseff <Aiseff@DESKTOP-9EFAG55>
2023-06-28 16:57:18 +0300

    Описаны и реализованы базовые методы класса ball

--- ball.cpp
+++ ball.cpp
@@ -1,2 +1,52 @@
-#pragma once
 #include <iostream>
+#include "ball.h"
+double ball::Getradius() {
+	return radius;
+}
+double ball::Getx() {
+	return x;
+}
+double ball::Gety() {
+	return y;
+}
+double ball::Getz() {
+	return z;
+}
+
+double ball::GetxQ() {
+	return xQ;
+}
+double ball::GetyQ() {
+	return yQ;
+}
+double ball::GetzQ() {
+	return zQ;
+}
+
+
+void ball::Setradius(double valueradius) {
+	radius = valueradius;
+}
+void ball::Setx(double valuex) {
+	x = valuex;
+}
+void ball::Sety(double valuey) {
+	y = valuey;
+}
+void ball::Setz(double valuez) {
+	z = valuez;
+}
+
+void ball::SetxQ(double valuexQ) {
+	xQ = valuexQ;
+}
+void ball::SetyQ(double valueyQ) {
+	yQ = valueyQ;
+}
+void ball::SetzQ(double valuezQ) {
+	zQ = valuezQ;
+}
+
+void ball::show() {
+	cout << "X = " << x << "\nY = " << y << "\nZ = " << z << "\nRadius= " << radius << "Xq = " << xQ << "\nYq = " << yQ << "\nZq = " << zQ << endl << endl;
+}
--- ball.h
+++ ball.h
@@ -1,2 +1,34 @@
-#pragma once
-#include <iostream>
+﻿#pragma once
+using namespace std;
+class ball
+{
+private:
+
+	double radius;		//Радиус
+	double square, volume;		//Площадь, Объём
+	double x, y, z;		//Задание координат центральной точки
+	double xQ, yQ, zQ;		//Задание координат точки, лежащей на поверхности
+	
+public:
+
+	double Getradius();
+	double Getx();
+	double Gety();
+	double Getz();
+
+	double GetxQ();
+	double GetyQ();
+	double GetzQ();
+
+
+	void Setradius(double valueradius);
+	void Setx(double valuex);
+	void Sety(double valuey);
+	void Setz(double valuez);
+
+	void SetxQ(double valuexQ);
+	void SetyQ(double valueyQ);
+	void SetzQ(double valuezQ);
+
+	void show();
+};
