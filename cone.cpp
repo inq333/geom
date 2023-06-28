d8067119be91ae4b150f7dff06f280e1ee2785ce
Edmonboy <137922197+Edmonboy@users.noreply.github.com>
2023-06-28 16:10:53 +0300

    Update cone.cpp
    Добавил комментарии к методам класса Cone

--- cone.cpp
+++ cone.cpp
@@ -8,13 +8,13 @@ Cone::Cone(double cone_radius, double cone_height) {
 std::tuple<double, double, double> Cone::vertex() {
     return std::make_tuple(0.0, 0.0, cone_height);
 }
-double Cone::cone_base_area() {
+double Cone::cone_base_area() {     //считает площадь основания конуса
     return M_PI * pow(cone_radius, 2);
 }
-double Cone::cone_lateral_area() {
-    double slant_height = sqrt(pow(cone_radius, 2) + pow(cone_height, 2));
+double Cone::cone_lateral_area() {     //считает площадь поверхности конуса
+    double slant_height = sqrt(pow(cone_radius, 2) + pow(cone_height, 2));     //длина стороны конуса(гипотенуза)
     return M_PI * cone_radius * slant_height;
 }
-double Cone::cone_volume() {
+double Cone::cone_volume() {     //считает объем конуса
     return (1.0 / 3.0) * M_PI * pow(cone_radius, 2) * cone_height;
 }
