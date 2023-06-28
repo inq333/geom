442baaed40c73187c783964db76ffdd0d34347fb
Edmonboy <137922197+Edmonboy@users.noreply.github.com>
2023-06-28 15:41:35 +0300

    Update cone.h
    ya daun

--- cone.h
+++ cone.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <tuple>
+
+class Cone {
+private:
+    double cone_radius;
+    double cone_height;
+public:
+    Cone(double cone_radius, double cone_height);
+    std::tuple<double, double, double> vertex();
+    double cone_base_area();
+    double cone_lateral_area();
+    double cone_volume();
+};
