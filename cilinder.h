09f86d9dc0edf0a4a08505f3686470f0a7b9aa63
Aloneesh <137945533+Aloneesh@users.noreply.github.com>
2023-07-01 00:03:39 +0300

    Update cilinder.h

--- cilinder.h
+++ cilinder.h
@@ -1,2 +1,23 @@
 #pragma once
-#include <algorithm>
+using namespace std;
+class cylinder
+{
+private:
+
+  double radius, h;    //Радиус, Высота цилиндра
+  double square, volume;    //Площадь, Объём
+  
+public:
+
+  double Getradius();
+  double Geth();
+
+  double Getradiusxyz();    //Расчёт радиуса в случае задания по координатам
+  double Getsquare();    //Расчёт площади цилиндра
+  double Getvolume();    //Расчёт объёма цилиндра
+
+  void Setradius(double valueradius);
+  void Seth(double valueh);
+
+  void Show();
+};
