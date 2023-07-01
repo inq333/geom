5dd054a8a9b25b106afe906d5296e863238daedf
Aloneesh <137945533+Aloneesh@users.noreply.github.com>
2023-07-01 20:44:24 +0300

    Update cilinder.h

--- cilinder.h
+++ cilinder.h
@@ -6,18 +6,38 @@ private:
 
   double radius, h;    //Радиус, Высота цилиндра
   double square, volume;    //Площадь, Объём
+  double x, y, z;    //Задание координат центральной точки
+  double xh, yh, zh;    //Задание координат точки, лежащей в центре одного из оснований 
+  double xQ, yQ, zQ;    //Задание координат точки, лежащей в одной плоскости с центром цилиндра, параллельной основанию, на поверхности 
   
 public:
 
-  double Getradius();
-  double Geth();
+  float Getradius();
+  float Geth();
+  float Getx();
+  float Gety();
+  float Getz();
+  float Getxh();
+  float Getyh();
+  float Getzh();
+  float GetxQ();
+  float GetyQ();
+  float GetzQ();
 
   double Getradiusxyz();    //Расчёт радиуса в случае задания по координатам
   double Getsquare();    //Расчёт площади цилиндра
   double Getvolume();    //Расчёт объёма цилиндра
 
-  void Setradius(double valueradius);
-  void Seth(double valueh);
-
-  void Show();
+  void Setradius(float valueradius);
+	void Setx(float valuex);
+	void Sety(float valuey);
+	void Setz(float valuez);
+  void Seth(float valueh);
+	void SetxQ(float valuexQ);
+	void SetyQ(float valueyQ);
+	void SetzQ(float valuezQ);
+  void Setxh(float valuexh);
+	void Setyh(float valueyh);
+	void Setzh(float valuezh);
+	void Show();
 };
