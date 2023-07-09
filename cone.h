6f1ea86a7125efe3dcadba2c896a7dca1d144b04
inq333 <96833730+inq333@users.noreply.github.com>
2023-07-09 14:23:39 +0300

    Update cone.h

--- cone.h
+++ cone.h
@@ -10,6 +10,10 @@ public:
     Cone();
     Cone(const Cone& obj);
     ~Cone();
+    double get_radius();
+    double get_height();
+    void set_radius(double);
+    void set_height(double);
     std::tuple<double, double, double> vertex();
     double cone_base_area();
     double cone_lateral_area();
