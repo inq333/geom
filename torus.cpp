fcf2cbe870a16172ddbe69113cfda4c4ff81d47d
inq333 <96833730+inq333@users.noreply.github.com>
2023-07-09 14:24:09 +0300

    Update torus.cpp

--- torus.cpp
+++ torus.cpp
@@ -1,39 +1,33 @@
 #include <iostream>
 #include <cmath>
-#include "Torus.h"
-#define PI 3,1415926535
+#include "torus.h"
+#define PI 3.1415926535
 
 using namespace std;
 
-Torus::Torus() :  volume(0.0), radius(0.0), surface_area(0.0), distance(0.0) {}
-Torus::Torus(double volume, double radius, double surface_area, double distance)
+Torus::Torus() : radius(0.0), distance(0.0) {}
+Torus::Torus(float radius, float distance)
 {
-	this->volume = volume;
-	this->radius = radius;
-	this->surface_area = surface_area;
-	this->distance = distance;
+    this->radius = radius;
+    this->distance = distance;
 }
 Torus::Torus(const Torus& obj)
 {
-	this->volume = obj.volume;
-	this->radius = obj.radius;
-	this->surface_area = obj.surface_area;
-	this->distance = obj.distance;
+    this->radius = obj.radius;
+    this->distance = obj.distance;
 }
 Torus::~Torus() {}
-double Torus::get_volume() { return volume = 2 * PI * PI * distance * radius * radius; } // Рассчет объёма тора по формуле 
-double Torus::get_radius() { return radius; }
-double Torus::get_surface_area() { return surface_area = 4 * PI * PI * radius * distance; } // Рассчет площади поверхности тора по формуле
-double Torus::get_distance() { return distance; }
+float Torus::get_radius() { return radius; }
+float Torus::get_distance() { return distance; }
 
-void Torus::set_volume(double volume) { this->volume = volume; }
-void Torus::set_radius(double radius) { this->radius = radius; }
-void Torus::set_surface_area(double surface_area) { this->surface_area = surface_area; }
-void Torus::set_distance(double distance) { this->distance = distance; }
+void Torus::set_radius(float radius) { this->radius = radius; }
+void Torus::set_distance(float distance) { this->distance = distance; }
 
-void Torus::show() {
-	cout << "Torus radius: " << radius << endl;
-	cout << "Volume of a torus: " << volume << endl;
-	cout << "Distance to the central axis: " << distance << endl;
-	cout << "Surface area of a torus: " << surface_area << endl;
+double Torus::surface_area() {return 4 * PI * PI * radius * distance;}
+double Torus::volume() {return 2 * PI * PI * distance * radius * radius;}
+
+void Torus::show()
+{
+    cout << "Torus radius: " << radius << endl;
+    cout << "Distance to the central axis: " << distance << endl;
 }
